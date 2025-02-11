5be26d04cdae970d25a05dfcc424d85069668582
Yam Peleg <ypeleg2@gmail.com>
2025-02-11 17:50:44 +0200

    added mongodb url

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,5 @@
 export default  {
-  dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: 'mongodb://127.0.0.1:27017',  
+  // dbURL: 'mongodb+srv://user:12345678a@cluster0.joqz6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   dbName : 'tester_db'
 }
