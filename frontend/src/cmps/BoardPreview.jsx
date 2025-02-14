7cc317cd211bd2dc1c7cad10dbb24910f2540cdb
Tal Shiri <talshiri122@gmail.com>
2025-02-14 10:45:40 +0800

    Finish stars f

--- frontend/src/cmps/BoardPreview.jsx
+++ frontend/src/cmps/BoardPreview.jsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom'
+import { updateBoard } from "../store/store.js"
 
 
 export function BoardPreview({ board }) {
-    console.log(board.style.backgroundImage);
+
+    async function setIsStarred(ev) {
+        ev.stopPropagation()
+        board.isStarred = !board.isStarred
+        console.log(board.isStarred);
+
+        const boardToSave = await updateBoard(board)
+        console.log(boardToSave);
+
+    }
 
     return <article className="board-preview" style={{ backgroundImage: `url(${board.style.backgroundImage})` }}>
         <h4>{board.title}</h4>
+        {!board.isStarred ? (
+            <div className="fa-solid fa-star star-btn" onClick={setIsStarred} > </div>
+        ) : (
+            <div className="fill-star"
+                onClick={setIsStarred}
+            >
+                <img src="star-solid.svg" alt="" />
+            </div>
+        )
+        }
 
-        <div className="fa-solid fa-star star-btn"></div>
     </article>
 }
