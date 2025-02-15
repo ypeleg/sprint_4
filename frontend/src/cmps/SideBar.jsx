d8f344f7daeeb11cfdbd25200f083962a21f42f9
Yam Peleg <ypeleg2@gmail.com>
2025-02-15 16:18:29 +0200

    some more css fixes

--- frontend/src/cmps/SideBar.jsx
+++ frontend/src/cmps/SideBar.jsx
@@ -62,7 +62,7 @@ export function SideBar() {
                 </article>
             </section>
 
-            <button className="label"> PREMIUM</button>
+            <button className="label"> PREMIUM </button>
 
             <section>
                 <div className="side-section-header">
@@ -73,14 +73,16 @@ export function SideBar() {
 
                     <div className="side-bar-item side-bar-sec-items flex-space-between  nav-highlight-hint">
                         <span>
-                            <i className="fa-regular fa-table"></i> Table
+                            <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.66683 9.66665C0.93045 9.66665 0.333496 9.06969 0.333496 8.33331V1.66665C0.333496 0.930267 0.93045 0.333313 1.66683 0.333313H12.3335C13.0699 0.333313 13.6668 0.930267 13.6668 1.66665V8.33331C13.6668 9.06969 13.0699 9.66665 12.3335 9.66665H1.66683ZM12.3335 5.66665V4.33331H5.66683V5.66665H12.3335ZM12.3335 2.99998V1.66665H5.66683V2.99998H12.3335ZM12.3335 6.99998V8.33331H5.66683V6.99998H12.3335ZM1.66683 4.33331V5.66665H4.3335V4.33331H1.66683ZM1.66683 6.99998V8.33331H4.3335V6.99998H1.66683ZM1.66683 2.99998V1.66665H4.3335V2.99998H1.66683Z" fill="currentColor"></path></svg>
+                             Table
                         </span>
                         <span></span>
                     </div>
 
                     <div className="side-bar-item side-bar-sec-items flex-space-between  nav-highlight-hint">
                         <span>
-                            <i className="fa-regular fa-calendar"></i> Calendar
+                            <svg width="16" height="16" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M6 4V5H4.995C3.892 5 3 5.893 3 6.994V19.006C3 20.106 3.893 21 4.995 21H19.005C20.108 21 21 20.107 21 19.006V6.994C21 5.895 20.107 5 19.005 5H18V4C18 3.448 17.552 3 17 3C16.448 3 16 3.448 16 4V5H8V4C8 3.448 7.552 3 7 3C6.448 3 6 3.448 6 4ZM5.25 9.571V17.718C5.25 18.273 5.694 18.714 6.243 18.714H17.758C18.3 18.714 18.75 18.268 18.75 17.718V9.571H5.25ZM9 13V10.999H7V13H9ZM17 10.999V13H15V10.999H17ZM11 13H13.001V10.999H11V13ZM7 17V15H9V17H7ZM11 17H13.001V15H11V17ZM17 15V17H15V15H17Z" fill="currentColor"></path></svg>
+                             Calendar
                         </span>
                         <span></span>
                     </div>
