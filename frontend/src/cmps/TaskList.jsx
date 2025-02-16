7a88bdef18a281f912d2213bbf3101dbae58cfc2
lironhefcode <lironheftman@gmail.com>
2025-02-16 18:04:00 +0200

    added task svg

--- frontend/src/cmps/TaskList.jsx
+++ frontend/src/cmps/TaskList.jsx
@@ -314,10 +314,12 @@ export function TaskList({
                         </div>
 
                         <div className="stay-same-height">
-                            <div
-                                className={`task-checkbox${task.status === "done" ? " checked" : ""}`}
+                           
+                            {(task.status === "done") ? <svg style={{color:'rgb(34, 160, 107)'}} onClick={(ev) => onToggleDone(ev, task)} width='16' height='16' fill="none" viewBox="0 0 16 16" role="presentation" class="css-1t4wpzr"><path fill="currentcolor" fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m12.326-2.52-1.152-.96L6.75 9.828 4.826 7.52l-1.152.96 2.5 3a.75.75 0 0 0 1.152 0z" clip-rule="evenodd"></path></svg>: <div
+                                className={`task-checkbox`}
+                                
                                 onClick={(ev) => onToggleDone(ev, task)}
-                            />
+                            />}
                             <span className="task-task-text">{task.title}</span>
                         </div>
 
