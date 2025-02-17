9924044c551b11e0e3df2ed04975f010aa3ca87e
Yam Peleg <ypeleg2@gmail.com>
2025-02-17 09:33:53 +0200

    tiny cleanup

--- frontend/src/cmps/GroupList.jsx
+++ frontend/src/cmps/GroupList.jsx
@@ -1,37 +1,39 @@
-import { useState } from "react"
-import { useSelector } from "react-redux"
-import { GroupHeader } from "./GroupHeader"
-import { TaskList } from "./TaskList"
-import { AddGroup } from "./AddGroup"
 
 
+import {useState} from "react"
+import {TaskList} from "./TaskList"
+import {AddGroup} from "./AddGroup"
+import {useSelector} from "react-redux"
+import {GroupHeader} from "./GroupHeader"
 
-export function GroupList({onLoadTask,onMoveCard, onReorderCard, Placeholder, onSetPlaceholderHeight, placeholderHeight}){
-     const boardToShow = useSelector(state => state.boardModule.board)
-     const [largeLabels, setLargeLabels] = useState(false)
-    
-        function toggleLargeLabels(ev) {
-            ev.stopPropagation()
-            setLargeLabels(!largeLabels)
-        }
-    return(
+
+export function GroupList({onLoadTask, onMoveCard, onReorderCard, Placeholder, onSetPlaceholderHeight, placeholderHeight}) {
+    const boardToShow = useSelector(state => state.boardModule.board)
+    const [largeLabels, setLargeLabels] = useState(false)
+
+    function toggleLargeLabels(ev) {
+        ev.stopPropagation()
+        setLargeLabels(!largeLabels)
+    }
+
+    return (
         <section className="group-lists">
-                                {boardToShow.groups.map(group => {
-        
-                                    // return <GroupPreview currentBoard={boardToShow} onLoadTask={onLoadTask} group={group}/>
-                                    return <div className="list base-components-list" style={{backgroundColor: (group.style?.backgroundColor || ''), color: (group.style?.color || '#172b4d')}}>
-                                    <GroupHeader group={group}/>
-                                        <TaskList
-                                            onSetPlaceholderHeight={onSetPlaceholderHeight} Placeholder={Placeholder} placeholderHeight={placeholderHeight}
-                                            toggleLargeLabels={toggleLargeLabels} largeLabels={largeLabels} currentBoard={boardToShow} currentGroup={group} onLoadTask={onLoadTask} group={group}
-                                                  onMoveCard={onMoveCard} onReorderCard={onReorderCard}/>
-        
-        
-                                    </div>
-        
-        
-                                })}
-                                <AddGroup/>
-                            </section>
+            {boardToShow.groups.map(group => {
+
+                // return <GroupPreview currentBoard={boardToShow} onLoadTask={onLoadTask} group={group}/>
+                return <div className="list base-components-list" style={{backgroundColor: (group.style?.backgroundColor || ''), color: (group.style?.color || '#172b4d')}}>
+                    <GroupHeader group={group}/>
+                    <TaskList
+                        onSetPlaceholderHeight={onSetPlaceholderHeight} Placeholder={Placeholder} placeholderHeight={placeholderHeight}
+                        toggleLargeLabels={toggleLargeLabels} largeLabels={largeLabels} currentBoard={boardToShow} currentGroup={group} onLoadTask={onLoadTask} group={group}
+                        onMoveCard={onMoveCard} onReorderCard={onReorderCard}/>
+
+
+                </div>
+
+
+            })}
+            <AddGroup/>
+        </section>
     )
 }
