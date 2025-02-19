50b2c1667a9b16cd5f95dca30bdb7e5db4d3386a
lironhefcode <lironheftman@gmail.com>
2025-02-20 01:21:28 +0200

    add color

--- frontend/src/cmps/ActivityMenu.jsx
+++ frontend/src/cmps/ActivityMenu.jsx
@@ -26,7 +26,13 @@ const imageUrls = [
     "https://images.unsplash.com/photo-1736855719599-c0298b8faf95?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MDY2fDB8MXxjb2xsZWN0aW9ufDI2fDMxNzA5OXx8fHx8Mnx8MTczOTk4NzkwNXw&ixlib=rb-4.0.3&q=80&w=1000",
     "https://images.unsplash.com/photo-1735527919007-3ba8d909049e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MDY2fDB8MXxjb2xsZWN0aW9ufDI4fDMxNzA5OXx8fHx8Mnx8MTczOTk4NzkwNXw&ixlib=rb-4.0.3&q=80&w=1000"
 ];
-
+const colors = [
+    "https://trello.com/assets/13425f9db06517de0f7f.svg",
+    "https://trello.com/assets/707f35bc691220846678.svg",
+    "https://trello.com/assets/d106776cb297f000b1f4.svg",
+    "https://trello.com/assets/8ab3b35f3a786bb6cdac.svg",
+    "https://trello.com/assets/a7c521b94eb153008f2d.svg"
+  ]
 
 export function ActivityMenu({ onSetActivityMenu }) {
 
@@ -35,7 +41,13 @@ export function ActivityMenu({ onSetActivityMenu }) {
     const [mode, setMode] = useState('main')
 
   function changeBoardBackGround(idx){
-    board.style.backgroundImage = imageUrls[idx]
+    if(mode === 'backgroundimages' ){
+
+        board.style.backgroundImage = imageUrls[idx]
+    }else{
+        board.style.backgroundImage = colors[idx]
+    }
+
     updateBoard(board)
   }
 
@@ -112,7 +124,7 @@ function ChnageBackGroud({ board, onSetActivityMenu, setMode }) {
 
         <div className="backGroundoptions">
             <div onClick={() => {setMode('backgroundimages')}} className="photo border"><img src="https://trello.com/assets/8f9c1323c9c16601a9a4.jpg" alt="" /><span>Photos</span></div>
-            <div className="photo border"><img src="https://trello.com/assets/97db30fe74a58b7b7a18.png" alt="" /><span>Colors</span></div>
+            <div onClick={() => {setMode('backgroundcolors')}} className="photo border"><img src="https://trello.com/assets/97db30fe74a58b7b7a18.png" alt="" /><span>Colors</span></div>
             <div className="addimagecontianire">
                 <span className="custom">Custom</span>
                 <div className="addimage">
@@ -131,7 +143,7 @@ function BackgroundImages({changeBoardBackGround, board, onSetActivityMenu, setM
     return (<>
     <div className="header">
                 <button onClick={() => setMode('main')}><svg width="20" height="20" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M7.29289 11.2929L14.364 4.22185C14.7545 3.83132 15.3876 3.83132 15.7782 4.22185C16.1687 4.61237 16.1687 5.24554 15.7782 5.63606L9.41421 12L15.7782 18.364C16.1687 18.7545 16.1687 19.3877 15.7782 19.7782C15.3877 20.1687 14.7545 20.1687 14.364 19.7782L7.29289 12.7071C6.90237 12.3166 6.90237 11.6834 7.29289 11.2929Z" fill="currentColor"></path></svg></button>
-                <span className="title">Change background</span>
+                <span className="title">photos</span>
                 <button onClick={onSetActivityMenu}><svg width="20" height="20" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12Z" fill="currentColor"></path></svg></button>
             </div >
 
@@ -146,6 +158,25 @@ function BackgroundImages({changeBoardBackGround, board, onSetActivityMenu, setM
 }
 
 
+function ColorBackgroundPicker({changeBoardBackGround,onSetActivityMenu,setMode}){
+    
+    return (<>
+        <div className="header">
+                    <button onClick={() => setMode('main')}><svg width="20" height="20" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M7.29289 11.2929L14.364 4.22185C14.7545 3.83132 15.3876 3.83132 15.7782 4.22185C16.1687 4.61237 16.1687 5.24554 15.7782 5.63606L9.41421 12L15.7782 18.364C16.1687 18.7545 16.1687 19.3877 15.7782 19.7782C15.3877 20.1687 14.7545 20.1687 14.364 19.7782L7.29289 12.7071C6.90237 12.3166 6.90237 11.6834 7.29289 11.2929Z" fill="currentColor"></path></svg></button>
+                    <span className="title">Colors</span>
+                    <button onClick={onSetActivityMenu}><svg width="20" height="20" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12Z" fill="currentColor"></path></svg></button>
+                </div >
+    
+                <div className="photooptions">
+                    
+                        {colors.map((url,index) =>{
+                            return <div onClick={() => {changeBoardBackGround(index)}} key={index} className="photo "><img src={url} alt="" /></div> 
+    
+                        })}
+                </div>
+        </>)
+}
+
 function Dynmic({changeBoardBackGround, mode, board, onSetActivityMenu, setMode }) {
 
 
@@ -156,7 +187,10 @@ function Dynmic({changeBoardBackGround, mode, board, onSetActivityMenu, setMode 
             return <ChnageBackGroud board={board} setMode={setMode} onSetActivityMenu={onSetActivityMenu} />
         case 'backgroundimages':
             return <BackgroundImages changeBoardBackGround={changeBoardBackGround} board={board} setMode={setMode} onSetActivityMenu={onSetActivityMenu} />
-        default:
+        
+        case 'backgroundcolors':
+            return <ColorBackgroundPicker changeBoardBackGround={changeBoardBackGround} board={board} setMode={setMode} onSetActivityMenu={onSetActivityMenu}/>
+            default:
             return <MainMenu board={board} setMode={setMode} onSetActivityMenu={onSetActivityMenu} />
     }
 
