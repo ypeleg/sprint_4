b6e736e450326a15dc35d090b8505096e639d9ee
lironhefcode <lironheftman@gmail.com>
2025-02-22 23:53:54 +0200

    google map last fix

--- frontend/src/pages/BoardDetails.jsx
+++ frontend/src/pages/BoardDetails.jsx
@@ -17,7 +17,7 @@ import {ShareModal} from "../cmps/ShareModal.jsx"
 
 
 // import GoogleMapReact from 'google-map-react'
-import {StandaloneSearchBox, useJsApiLoader,GoogleMap  } from '@react-google-maps/api'
+import {StandaloneSearchBox, useJsApiLoader,GoogleMap, Marker  } from '@react-google-maps/api'
 import {ActivityMenu} from "../cmps/ActivityMenu.jsx"
 import { UPDATE_BOARD } from "../store/reducers/board.reducer.js"
 
@@ -1074,16 +1074,16 @@ export function Map({ name, lat= 32.109333, lng= 34.855499, zm }) {
       
     return (<div className="maps-container maps-container-outer">
             <div className="maps-in-1" style={{height: '160px', width: '100%'}}>
-                <GoogleMap mapContainerStyle={{ width: '100%', height: '100%' }}  center={{lat,lng}} zoom={10} > <AnyReactComponent text={'📍'}></AnyReactComponent> </GoogleMap>
+                <GoogleMap mapContainerStyle={{ width: '100%', height: '100%' }}  center={{lat,lng}} zoom={10} > <Marker position={{lat,lng}} ></Marker> </GoogleMap>
             </div>
             <div className="maps-in-2" style={{height: '52px', width: '512px'}}>
                 <h3>{name || 'Tel Aviv'}</h3>
-                <h5>Tel Aviv-Yafo, Israel</h5>
+               
             </div>
         </div>)
 }
 
-const AnyReactComponent = ({text}) => <div style={{fontSize: '22px'}}>{text}</div>
+const AnyReactComponent = ({text}) => <div style={{fontSize: '22px', position: 'absolute' }}>{text}</div>
 
 
 export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
@@ -1919,7 +1919,7 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
                                 </div>
                             </div>
 
-                            {/* Maps */} {showMaps && <div className="task-section">
+                            {/* Maps */} {(!!taskToShow.location) && <div className="task-section">
                             <div className="flex-space-between">
                                 <div className="section-icon-title">
                                     <i>
