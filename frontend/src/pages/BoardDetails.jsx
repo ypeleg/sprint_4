032c398128092942c0f02a280419b568d9bed65e
Yam Peleg <ypeleg2@gmail.com>
2025-02-27 02:56:18 +0200

    build

--- frontend/src/pages/BoardDetails.jsx
+++ frontend/src/pages/BoardDetails.jsx
@@ -1264,10 +1264,19 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
 
     // labels
     function onAddChecklist(ev) {
+
+        hidePicker(ev)
+
+        const checklistId = Date.now()
+
         setChecklists(prev => [...prev, {
-            id: Date.now(), title: newChecklistTitle, todos: []
+            id: checklistId, title: newChecklistTitle, todos: []
         }])
         setNewChecklistTitle('')
+
+        setAddingToChecklist(checklistId)
+        // elTextArea.current.focus()
+
         hidePicker(ev)
         // elTextArea
         // elTextArea.current.focus()
@@ -1755,6 +1764,7 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
 
 
     const elFuckingButton = useRef()
+    const elFuckingButton2 = useRef()
 
     
     const elInnerChecklistRef = useRef()
@@ -1802,8 +1812,15 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
     
 
     function addChecklistItem(ev) {
-        elFuckingButton.current.click()
-        elTextArea?.current?.focus()
+        try {
+            elFuckingButton.current.click()
+            elTextArea?.current?.focus()
+        } catch (err) {
+            elFuckingButton2.current.click()
+            elTextArea?.current?.focus()
+        }
+
+
     }
 
 
@@ -1816,8 +1833,19 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
 
             if (ev.key === "Enter") {
                 ev.preventDefault()
+                ev.stopPropagation()
 
-                addChecklistItem(ev)
+                try {
+                    elFuckingButton.current.click()
+                    elTextArea?.current?.focus()
+                } catch (err) {
+                    elFuckingButton2.current.click()
+                    elTextArea?.current?.focus()
+                }
+
+                // elFuckingButton.current.click()
+                // elTextArea?.current?.focus()
+                // addChecklistItem(ev)
 
                 // await onSubmit(ev)
             }
@@ -2155,7 +2183,7 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
 
                                         {(addingToChecklist === checklist.id) && <>
                                             <div className="task-checklist-add inner-component-left-padding">
-                                                <input data-checklistid={checklist.id} ref={elTextArea} className="checklist-input" type="text" placeholder="Add an item" value={newChecklistItem} onChange={(e) => setNewChecklistItem(e.target.value)}/>
+                                                <input autoFocus data-checklistid={checklist.id} ref={elTextArea} className="checklist-input" type="text" placeholder="Add an item" value={newChecklistItem} onChange={(e) => setNewChecklistItem(e.target.value)}/>
                                             </div>
 
                                             <div className="side-by-side inner-component-left-padding">
@@ -2296,9 +2324,9 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
                                 hidePicker(event)
                                 movePickerTo(event)
                                 setShowPickerChecklists(true)
-                                console.log(elInnerChecklistRef)
-                                elInnerChecklistRef.current.focus()
-                                console.log('after')
+                                // console.log(elInnerChecklistRef)
+                                // elInnerChecklistRef.current.focus()
+                                // console.log('after')
                             }}>
                                 <svg width="24" height="24" role="presentation" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd" clipRule="evenodd" d="M6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V13C20 12.4477 19.5523 12 19 12C18.4477 12 18 12.4477 18 13V18H6V6L16 6C16.5523 6 17 5.55228 17 5C17 4.44772 16.5523 4 16 4H6ZM8.73534 10.3223C8.36105 9.91618 7.72841 9.89038 7.3223 10.2647C6.91619 10.639 6.89039 11.2716 7.26467 11.6777L10.8768 15.597C11.4143 16.1231 12.2145 16.1231 12.7111 15.6264L13.0754 15.2683C13.3699 14.9785 13.6981 14.6556 14.0516 14.3075C15.0614 13.313 16.0713 12.3169 17.014 11.3848L17.0543 11.3449C18.7291 9.68869 20.0004 8.42365 20.712 7.70223C21.0998 7.30904 21.0954 6.67589 20.7022 6.28805C20.309 5.90022 19.6759 5.90457 19.2881 6.29777C18.5843 7.01131 17.3169 8.27244 15.648 9.92281L15.6077 9.96263C14.6662 10.8937 13.6572 11.8889 12.6483 12.8825L11.8329 13.6851L8.73534 10.3223Z" fill="currentColor"></path>
@@ -2668,7 +2696,7 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
                 <div className="checklist-content">
 
                     <div className="title-section">
-                        <label>Title</label> <input ref={elInnerChecklistRef} type="text" className="title-input" value={newChecklistTitle} onChange={(ev) => {
+                        <label>Title</label> <input autoFocus ref={elInnerChecklistRef} type="text" className="title-input" value={newChecklistTitle} onChange={(ev) => {
                         setNewChecklistTitle(ev.target.value)
 
                     }}/>
@@ -2685,7 +2713,7 @@ export function TaskModal({taskToShow, onClose, popupRef, onSaveTaskOuter}) {
                         </div>
                     </div>
 
-                    <button className="add-checklist-btn" onClick={onAddChecklist}>Add
+                    <button ref={elFuckingButton2} className="add-checklist-btn" onClick={onAddChecklist}>Add
                     </button>
                 </div>
             </div>
